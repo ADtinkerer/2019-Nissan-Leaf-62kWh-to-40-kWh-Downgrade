0b275e143e7c231f13138b73cb52a436ffa03509
ADtinkerer <149444574+ADtinkerer@users.noreply.github.com>
2024-11-09 14:40:07 -0500

    Update can-bridge-firmware.c
    commenting for changes to code.

--- can-bridge-firmware.c
+++ can-bridge-firmware.c
@@ -103,6 +103,24 @@ static CAN_FRAME	AZE0_393_message	= {.ID = 0x393, .dlc = 8, .ide = 0, .rtr = 0, 
 
 static uint8_t	crctable[256] = {0,133,143,10,155,30,20,145,179,54,60,185,40,173,167,34,227,102,108,233,120,253,247,114,80,213,223,90,203,78,68,193,67,198,204,73,216,93,87,210,240,117,127,250,107,238,228,97,160,37,47,170,59,190,180,49,19,150,156,25,136,13,7,130,134,3,9,140,29,152,146,23,53,176,186,63,174,43,33,164,101,224,234,111,254,123,113,244,214,83,89,220,77,200,194,71,197,64,74,207,94,219,209,84,118,243,249,124,237,104,98,231,38,163,169,44,189,56,50,183,149,16,26,159,14,139,129,4,137,12,6,131,18,151,157,24,58,191,181,48,161,36,46,171,106,239,229,96,241,116,126,251,217,92,86,211,66,199,205,72,202,79,69,192,81,212,222,91,121,252,246,115,226,103,109,232,41,172,166,35,178,55,61,184,154,31,21,144,1,132,142,11,15,138,128,5,148,17,27,158,188,57,51,182,39,162,168,45,236,105,99,230,119,242,248,125,95,218,208,85,196,65,75,206,76,201,195,70,215,82,88,221,255,122,112,245,100,225,235,110,175,42,32,165,52,177,187,62,28,153,147,22,135,2,8,141};
 
+// Code lifted from Dala's EV battery downgrade
+void setup() {
+	//put your setup code here to run once:
+}
+
+void loop() {
+	CAN.sendMsgBuf(0x1ED, 0, 3, 0xFF 0xE0 0x68);
+	delay(10);
+	CAN.sendMsgBuf(0x1ED, 0, 3, 0xFF 0xE1 0xED);
+	delay(10);
+	CAN.sendMsgBuf(0x1ED, 0, 3, 0xFF 0xE2 0xE7);
+	delay(10);
+	CAN.sendMsgBuf(0x1ED, 0, 3, 0xFF 0xE3 0x62);
+	delay(10);
+}
+
+// end of code lifted from Dala's EV battery downgrade
+
 void convert_array_to_5bc(Leaf_2011_5BC_message * dest, uint8_t * src);
 void calc_crc8(CAN_FRAME *frame);
 void reset_state(void);
@@ -787,6 +805,8 @@ void can_handler(uint8_t can_bus, CAN_FRAME *frame)
             }
             break;
 
+// This section needs to be updated for QC charging		
+		
         case 0x59E: // QC capacity message, adjust for AZE0 with 30/40/62kWh pack swaps
             if( My_Leaf == MY_LEAF_2014 )
             {
